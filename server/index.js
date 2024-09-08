c1f4bd7eb05ef12b51748ba4b9af375237b673c9
abhisheksharma <abhisheksharma131098@gmail.com>
2024-09-08 20:45:13 +0530

    created react app as frontend and setup redux toolkit and rdtk query

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 import serialRoutes from "./routes/serial.js";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json({ limit: "10kb", extended: true }));
 app.use(express.urlencoded({ limit: "10kb", extended: true }));
